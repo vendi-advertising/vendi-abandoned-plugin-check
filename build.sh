056b13dcc8c473186a3da2ed84bcec91ff73183c
Chris Haas <chris@vendiadvertising.com>
2019-04-24 08:27:20 -0500

    Use wp2md from composer

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
-if [ ! -d bin ]; then
-    mkdir bin
-fi
-
-if [ ! -f bin/wp2md ]; then
-    wget https://github.com/wpreadme2markdown/wp-readme-to-markdown/releases/download/2.0.2/wp2md.phar -O bin/wp2md
-    chmod a+x bin/wp2md
-fi
-
-./bin/wp2md convert < readme.txt > README.md
+./vendor/bin/wp2md convert < readme.txt > README.md
 
 php ~/wordpress-dev-tools/tools/i18n/makepot.php wp-plugin . languages/vendi-abandoned-plugin-check.pot
 
